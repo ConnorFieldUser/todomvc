5d818df23e2f84dbca56ab44ee804372f7ea7c43
ConnorFieldUser <conquest.field@gmail.com>
2016-10-25 13:51:08 -0400

    commit

--- todomvc/todo/views.py
+++ todomvc/todo/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+
+from todo.models import Todo
+from todo.serializers import TodoSerializer
 
 # Create your views here.
+
+
+class TodoListAPIView(ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
--- todomvc/todomvc/urls.py
+++ todomvc/todomvc/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
+
+from todo.views import TemplateView, TodoListAPIView
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^/api/todos/$', TodoListAPIView, name="todo_list_api_view")
 ]
